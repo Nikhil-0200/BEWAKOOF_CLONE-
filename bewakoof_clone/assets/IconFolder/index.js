cee970770f07e8e7a5bbeb5c9cc0ecd9aa68182f
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-03-29 17:55:26 +0530

    Images Stored

---
+++ bewakoof_clone/assets/IconFolder/index.js
@@ -0,0 +1,5 @@
+import navLogo from "./navLogo.svg"
+
+export {
+    navLogo
+}
---
+++ bewakoof_clone/assets/ImageFolder/index.js
@@ -0,0 +1,124 @@
+import slider1Peanut from "./Silder1Image/slider1Peanut.webp";
+import slider1Img from "./Silder1Image/slider1Img.gif";
+import slider1Img1 from "./Silder1Image/slider1Im1.gif";
+import slider1Img2 from "./Silder1Image/slider1Im2.gif";
+import slider1Img3 from "./Silder1Image/slider1Im3.gif";
+
+import slider1BelowImg from "./Slider1Below/img.gif";
+import slider1BelowImg1 from "./Slider1Below/img1.webp";
+import slider1BelowImg2 from "./Slider1Below/img2.jpg";
+import slider1BelowImg3 from "./Slider1Below/img3.webp";
+import slider1BelowImg4 from "./Slider1Below/img4.webp";
+import slider1BelowImg5 from "./Slider1Below/img5.gif";
+import slider1BelowImg6 from "./Slider1Below/img6.webp";
+import slider1BelowImg7 from "./Slider1Below/img7.webp";
+import slider1Banner from "./Slider1Below/slider1Banner.gif";
+
+import trendingImg from "./trendingImage/trendingImg.webp";
+import trendingImg1 from "./trendingImage/trendingImg1.webp";
+import trendingImg2 from "./trendingImage/trendingImg2.webp";
+import trendingImg3 from "./trendingImage/trendingImg3.webp";
+import trendingImg4 from "./trendingImage/trendingImg4.webp";
+import trendingImg5 from "./trendingImage/trendingImg5.webp";
+import trendingImg6 from "./trendingImage/trendingImg6.webp";
+import trendingImg7 from "./trendingImage/trendingImg7.webp";
+import trendingImg8 from "./trendingImage/trendingImg8.webp";
+import trendingImg9 from "./trendingImage/trendingImg9.webp";
+import trendingImg10 from "./trendingImage/trendingImg10.webp";
+import trendingImg11 from "./trendingImage/trendingImg11.webp";
+
+import slider2Img from "./Slider2Image/slider2Img.webp";
+import slider2Img1 from "./Slider2Image/slider2Img1.webp";
+import slider2Img2 from "./Slider2Image/slider2Img2.webp";
+import slider2Img3 from "./Slider2Image/slider2Img3.webp";
+import slider2Img4 from "./Slider2Image/slider2Img4.webp";
+import slider2Img5 from "./Slider2Image/slider2Img5.webp";
+
+import hotImg from "./HotImage/hotImg.jpg";
+import hotImg1 from "./HotImage/hotImg1.jpg";
+import hotImg2 from "./HotImage/hotImg2.jpg";
+import hotImg3 from "./HotImage/hotImg3.jpg";
+
+import catImg from "./CatImage/catImg.webp";
+import catImg1 from "./CatImage/catImg1.webp";
+import catImg2 from "./CatImage/catImg2.webp";
+import catImg3 from "./CatImage/catImg3.webp";
+import catImg4 from "./CatImage/catImg4.webp";
+import catImg5 from "./CatImage/catImg5.webp";
+
+import slider3Img from "./Slider3Image/slider3Img.jpg";
+import slider3Img1 from "./Slider3Image/slider3Img1.webp";
+import slider3Img2 from "./Slider3Image/slider3Img2.webp";
+import slider3Img3 from "./Slider3Image/slider3Img3.webp";
+
+import accImg from "./AccImage/accImg.webp";
+import accImg1 from "./AccImage/accImg1.webp";
+import accImg2 from "./AccImage/accImg2.webp";
+import accImg3 from "./AccImage/accImg3.webp";
+
+import bestImg from "./BestImage/bestImg.webp";
+import bestImg1 from "./BestImage/bestImg1.webp";
+import bestImg2 from "./BestImage/bestImg2.webp";
+import bestImg3 from "./BestImage/bestImg3.webp";
+
+import footerBanner1 from "./BestImage/footerBanner1.webp";
+import footerBanner2 from "./BestImage/footerBanner2.jpg";
+
+export {
+  slider1Peanut,
+  slider1Img,
+  slider1Img1,
+  slider1Img2,
+  slider1Img3,
+  slider1BelowImg,
+  slider1BelowImg1,
+  slider1BelowImg2,
+  slider1BelowImg3,
+  slider1BelowImg4,
+  slider1BelowImg5,
+  slider1BelowImg6,
+  slider1BelowImg7,
+  slider1Banner,
+  trendingImg,
+  trendingImg1,
+  trendingImg2,
+  trendingImg3,
+  trendingImg4,
+  trendingImg5,
+  trendingImg6,
+  trendingImg7,
+  trendingImg8,
+  trendingImg9,
+  trendingImg10,
+  trendingImg11,
+  slider2Img,
+  slider2Img1,
+  slider2Img2,
+  slider2Img3,
+  slider2Img4,
+  slider2Img5,
+  hotImg,
+  hotImg1,
+  hotImg2,
+  hotImg3,
+  catImg,
+  catImg1,
+  catImg2,
+  catImg3,
+  catImg4,
+  catImg5,
+  slider3Img,
+  slider3Img1,
+  slider3Img2,
+  slider3Img3,
+  accImg,
+  accImg1,
+  accImg2,
+  accImg3,
+  bestImg,
+  bestImg1,
+  bestImg2,
+  bestImg3,
+  footerBanner1,
+  footerBanner2,
+};
