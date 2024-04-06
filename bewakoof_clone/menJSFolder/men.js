f459f3fbcb4a8b72be8970f1ef2b60d85b2e8ea4
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-04-07 04:41:48 +0530

    Sorting Functionality Added

--- bewakoof_clone/menJSFolder/men.js
+++ bewakoof_clone/menJSFolder/men.js
@@ -1,70 +1,103 @@
-import {starLine} from "../assets/IconFolder/index"
+import { starLine } from "../assets/IconFolder/index";
 
 let API =
   "https://script.google.com/macros/s/AKfycbxCG90fekKd3afSynvi3JoCdyZG3nku--QWil_XpEQD_fMRjdY7f2ey6uUAgTHnQKdlYA/exec";
 
 const cardDiv = document.getElementById("cardDiv");
+const sort = document.querySelector("#sort");
 
 async function fetchData() {
   let res = await fetch(API);
   let finalData = await res.json();
 
-  // console.log(finalData);
+  // console.log(sort);
 
-  finalData.data.forEach((ele) => {
-    let itemCard = document.createElement("div");
-    itemCard.className = "itemCard";
-    itemCard.addEventListener("click", ()=>{
-      window.location.href = `singlePage.html?id=${ele.id}`
-    })
+  function renderData(data) {
+    cardDiv.innerHTML = "";
 
-    let imgDiv = document.createElement("div");
-    imgDiv.className = "imgDiv";
+    data.forEach((ele) => {
+      let itemCard = document.createElement("div");
+      itemCard.className = "itemCard";
+      itemCard.addEventListener("click", () => {
+        window.location.href = `singlePage.html?id=${ele.id}`;
+      });
 
-    let img = document.createElement("img");
-    img.src = ele.image1;
+      let imgDiv = document.createElement("div");
+      imgDiv.className = "imgDiv";
 
-    let ratingSpan = document.createElement("span");
-    ratingSpan.className = "ratingSpan";
+      let img = document.createElement("img");
+      img.src = ele.image1;
 
-    let ratingStar = document.createElement("img");
-    ratingStar.classList = "ratingStar"
-    ratingStar.src = starLine
+      let ratingSpan = document.createElement("span");
+      ratingSpan.className = "ratingSpan";
 
-    let rating = document.createElement("h3");
-    rating.className = "rating";
-    rating.textContent = ele.rating;
+      let ratingStar = document.createElement("img");
+      ratingStar.classList = "ratingStar";
+      ratingStar.src = starLine;
 
-    ratingSpan.append( ratingStar,rating)
+      let rating = document.createElement("h3");
+      rating.className = "rating";
+      rating.textContent = ele.rating;
 
-    let textData = document.createElement("div");
-    textData.className = "textData";
+      ratingSpan.append(ratingStar, rating);
 
-    let brandName = document.createElement("h3");
-    brandName.className = "brandName";
-    brandName.textContent = ele.brandName;
+      let textData = document.createElement("div");
+      textData.className = "textData";
 
-    let productName = document.createElement("h3");
-    productName.className = "productName";
-    productName.textContent = ele.productName
+      let brandName = document.createElement("h3");
+      brandName.className = "brandName";
+      brandName.textContent = ele.brandName;
 
-    let price = document.createElement("p");
-    price.className = "price";
-    price.textContent = ele.price;
+      let productName = document.createElement("h3");
+      productName.className = "productName";
+      productName.textContent = ele.productName;
 
-    let subTitle = document.createElement("span");
-    subTitle.className = "subTitle";
-    subTitle.textContent = ele.subTitle;
+      let price = document.createElement("p");
+      price.className = "price";
+      price.textContent = ele.price;
 
-    textData.append(brandName,
-        productName,
-        price,
-        subTitle)
+      let subTitle = document.createElement("span");
+      subTitle.className = "subTitle";
+      subTitle.textContent = ele.subTitle;
 
+      textData.append(brandName, productName, price, subTitle);
 
-    imgDiv.append(img);
-    itemCard.append(imgDiv, ratingSpan, textData);
-    cardDiv.append(itemCard);
+      imgDiv.append(img);
+      itemCard.append(imgDiv, ratingSpan, textData);
+      cardDiv.append(itemCard);
+    });
+  }
+
+  renderData(finalData.data);
+
+  sort.addEventListener("change", () => {
+    let sortData = [...finalData.data];
+
+    // console.log(sortData.price);
+
+    if (sort.value === "Popular") {
+      sortData.sort((a, b) => {
+        return a.id - b.id;
+      });
+    } else if (sort.value === "New") {
+      sortData.sort((a, b) => {
+        return b.id - a.id;
+      });
+    } else if (sort.value === "High to Low") {
+      sortData.sort((a, b) => {
+        let priceA = Number(a.price.replace(/[^\d.-]/g, ""));
+        let priceB = Number(b.price.replace(/[^\d.-]/g, ""));
+        return priceB - priceA;
+      });
+    } else if (sort.value === "Low to High")
+      sortData.sort((a, b) => {
+        let priceA = Number(a.price.replace(/[^\d.-]/g, ""));
+        let priceB = Number(b.price.replace(/[^\d.-]/g, ""));
+        return priceA - priceB;
+      });
+
+    console.log(sortData);
+    renderData(sortData);
   });
 }
 
@@ -82,4 +115,4 @@ heading2.textContent = "Nikhil";
 heading3.textContent = "Karan";
 heading4.textContent = "Aman";
 
-filterDiv.append(heading, heading2, heading3, heading4)
+filterDiv.append(heading, heading2, heading3, heading4);
