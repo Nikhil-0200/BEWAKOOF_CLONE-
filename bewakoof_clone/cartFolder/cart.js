22046f4da0b2bc47b365a691591c12a904287ea7
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-04-10 17:10:53 +0530

    Cart Total Functionality Made

--- bewakoof_clone/cartFolder/cart.js
+++ bewakoof_clone/cartFolder/cart.js
@@ -9,8 +9,8 @@ cartHeading.textContent = localStorageItem.length;
 console.log(localStorageItem);
 
 localStorageItem.forEach((element) => {
-    let container = document.createElement("div");
-    container.id = "container";
+  let container = document.createElement("div");
+  container.id = "container";
 
   let cartCard = document.createElement("div");
   cartCard.id = "cartCard";
@@ -27,7 +27,13 @@ localStorageItem.forEach((element) => {
 
   let cardPrice = document.createElement("h1");
   cardPrice.id = "cardPrice";
-  cardPrice.textContent = element.price;
+  cardPrice.textContent = `₹${element.price.replace(/₹/g, "")}`;
+
+  // totalMRP(element.price.replace(/₹/g, ""))
+
+  let quantityDropDown = count();
+
+  let sizeDropDown = size();
 
   let cardImgDiv = document.createElement("div");
   cardImgDiv.id = "cardImgDiv";
@@ -46,13 +52,87 @@ localStorageItem.forEach((element) => {
 
   cardBtnDiv.append(cardRemoveBtn, cardAddToWishBtn);
   cardImgDiv.append(cardImg);
-  cardLeft.append(cardProduct, cardPrice);
+  cardLeft.append(cardProduct, cardPrice, sizeDropDown, quantityDropDown);
   cardRight.append(cardImgDiv);
   cartCard.append(cardLeft, cardRight);
-  container.append(cartCard, cardBtnDiv)
+  container.append(cartCard, cardBtnDiv);
   bottom.append(container);
 });
 
 let data = JSON.parse(localStorage.getItem("setUserName"));
 
 userNameSpan.textContent = data.toString();
+
+function count() {
+  let quantity = document.createElement("select");
+  quantity.id = "quantity";
+
+  let arr = [
+    "Qty: 1",
+    "Qty: 2",
+    "Qty: 3",
+    "Qty: 4",
+    "Qty: 5",
+    "Qty: 6",
+    "Qty: 7",
+    "Qty: 8",
+    "Qty: 9",
+    "Qty: 10",
+  ];
+
+  for (let i = 0; i < arr.length; i++) {
+    let option = document.createElement("option");
+    option.textContent = arr[i];
+    quantity.append(option);
+  }
+  return quantity;
+}
+
+function size() {
+  let arr = [
+    "Size: S",
+    "Size: M",
+    "Size: L",
+    "Size: XL",
+    "Size: 2XL",
+    "Size: 3XL",
+  ];
+
+  let sizeSelect = document.createElement("select");
+  sizeSelect.id = "sizeSelect";
+
+  arr.forEach((ele) => {
+    let sizeOption = document.createElement("option");
+    sizeOption.textContent = ele;
+    sizeSelect.append(sizeOption);
+  });
+
+  return sizeSelect;
+}
+
+function totalMRP(){
+ let res = localStorageItem.reduce((acc, curr)=> acc + +(curr.price.replace(/₹/g, "")), 0)
+ return res
+}
+
+let totalMRPspan = document.getElementById("totalMRPspan");
+totalMRPspan.textContent = `₹${totalMRP()}`
+
+
+function gst(value){
+  let gstAmt = (value * 0.18)
+  return gstAmt
+}
+
+let gstAmtSpan = document.getElementById("gstAmtSpan");
+gstAmtSpan.textContent = `${gst(totalMRP())}`
+
+function subTotal(value1, value2){
+let res = value1 + value2
+return res
+}
+
+let subTotalValue = subTotal(totalMRP(), gst(totalMRP()))
+
+let subtotalSpan = document.getElementById("subtotalSpan");
+subtotalSpan.textContent = `₹${(subTotalValue).toFixed(2)}`
